93ebb236b088b6cccc4c07d4cd78204674223f5c
Cannonbait <ivar_busig@hotmail.com>
2015-02-26 19:58:33 +0100

    Changed storage of connected users from List to ordset

--- channel.erl
+++ channel.erl
@@ -13,25 +13,26 @@ main(State) ->
 
 initial_state(Id) ->
 	%When the server is started we have no users
-    #channel_st{users=[Id]}.
+	Users = ordsets:new(),
+    #channel_st{users= ordsets:add_element(Id, Users)}.
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 		INTERNAL
 
 %Put user in channel
 loop(State, {join, {Id}}) ->
-	{{join, ok}, State#channel_st{users = lists:sort([Id| State#channel_st.users])}};
+	{{join, ok}, State#channel_st{users = ordsets:add_element(Id, State#channel_st.users)}};
 
 %Recieve message from user
 loop(State, {message, {Nick, Id, Channel, Msg}}) ->
 	%Spawn new process to send messages
-	spawn(channel, sendMessage, [State#channel_st.users, {Channel, Nick, Msg, Id}]),
+	spawn(channel, sendMessage, [ordsets:to_list(State#channel_st.users), {Channel, Nick, Msg, Id}]),
 	%Acknowledge that server recieved message and it will be sent out
 	{{message, ok}, State};
 
 
 %User wants to leave a channel
 loop(State, {leave, {Id}}) ->
-	{{leave, ok}, State#channel_st{users = lists:delete(Id, State#channel_st.users)}}.
+	{{leave, ok}, State#channel_st{users = ordsets:del_element(Id, State#channel_st.users)}}.
 
 %Recursive function to send Message
 sendMessage([], _) -> ok;
